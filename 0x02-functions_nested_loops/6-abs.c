ebb5a23263659e8002af0e752c03906bf8cde931
Stones225 <ebrinroch@gmail.com>
2022-09-14 17:50:39 -0700

    My message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+*_abs - check the code
+*@n: the parameter
+*
+*Return: check the code
+*/
+
+int _abs(int n)
+{
+	return (abs(n));
+}
