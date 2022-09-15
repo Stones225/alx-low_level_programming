8dd921ec5d5926f38c94cbf602d8cc97935ce19f
Stones225 <ebrinroch@gmail.com>
2022-09-14 19:20:38 -0700

    My message

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - check the code
+*
+*Return: check the code
+*/
+
+int main(void)
+{
+
+	int sum = 0;
+	int i;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
