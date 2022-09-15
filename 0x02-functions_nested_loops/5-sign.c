088eba291a2b13445cd5934b87296145e0a83778
Stones225 <ebrinroch@gmail.com>
2022-09-14 17:44:59 -0700

    My message

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *print_sign - check the code
+ *@n: the parameter
+ *
+ *Return: check the code
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
