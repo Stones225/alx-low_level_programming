000e69c525d54e82f4296315d2becb382ee368bc
Stones225 <ebrinroch@gmail.com>
2022-09-16 16:30:28 -0700

    my message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *mul - check the code
+ *@a: the parameter
+ *@b: the parameter
+ *
+ *Return: check the code
+ */
+int mul(int a, int b)
+{
+
+return (a * b);
+}
